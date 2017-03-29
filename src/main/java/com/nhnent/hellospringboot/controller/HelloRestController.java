63ba0493e481d21ae5c77967309a5457e2a003dd
Dongho Sim <dhsim86@gmail.com>
2017-03-29 12:24:02 +0900

    Implemented example.
    Signed-off-by: Dongho Sim <dhsim86@gmail.com>

--- src/main/java/com/nhnent/hellospringboot/controller/HelloRestController.java
+++ src/main/java/com/nhnent/hellospringboot/controller/HelloRestController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nhnent.hellospringboot.dao.HelloDao;
@@ -36,7 +37,7 @@ public class HelloRestController {
         return helloData;
     }
     
-    @RequestMapping("/list")
+    @RequestMapping(path ="/list", method = RequestMethod.GET)
     public List<Hello> list(Model model) {
         
         List<Hello> helloList = helloDao.findAll();
